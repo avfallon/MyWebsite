ba8e7d61ef0a1ea04340d29b36ca81d9a0edfb94
avfallon <31700889+avfallon@users.noreply.github.com>
2023-11-15 21:42:53 -0700

    initial react

---
+++ src/service/MenuService.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+const myMenu = (
+    <ul>
+        <li>Home</li>
+        <li>About Andrew</li>
+        <li>LocaLibrary</li>
+    </ul>
+);
+
+
+
+
+root.render(myMenu);
